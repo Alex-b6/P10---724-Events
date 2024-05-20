c03ebf36c33d39eb7b1e095ace4367bf560af710
AlexB6 <brou.alexandre.pro@gmail.com>
2024-05-20 16:09:42 +0200

    Fix : fixing categories filters

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -15,8 +15,9 @@ const Select = ({
 }) => {
   const [value, setValue] = useState();
   const [collapsed, setCollapsed] = useState(true);
+
   const changeValue = (newValue) => {
-    onChange();
+    onChange(newValue);
     setValue(newValue);
     setCollapsed(newValue);
   };
@@ -58,6 +59,7 @@ const Select = ({
             e.preventDefault();
             setCollapsed(!collapsed);
           }}
+          aria-label="collapse button"
         >
           <Arrow />
         </button>
